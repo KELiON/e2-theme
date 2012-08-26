06ec57d8cba7bf3353821e259ee8aa4e19566c40
Alexandr Subbotin <kelionweb@gmail.com>
2012-08-27 00:04:07 +0400

    Initial commit

---
+++ templates/head-extras.tmpl.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="themes/subbotin/highlight/github.css">
+<script src="themes/subbotin/highlight/highlight.pack.js"></script>
+<script>
+	hljs.initHighlightingOnLoad();
+	$(window).load(function(){
+		$("body").addClass('loaded');
+	})
+</script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
